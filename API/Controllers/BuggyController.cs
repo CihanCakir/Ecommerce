ebf50073eb70331c3ec78936b67c4a1c23f539ff
Cihan Cakir <bencakirim61@gmail.com>
2020-04-10 03:13:55 +0300

    Error Hnadling Client Side COMP

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -12,32 +12,41 @@ namespace API.Controllers
             _context = context;
         }
 
+
         [HttpGet("notfound")]
         public ActionResult GetNotFoundRequest()
         {
-            return NotFound(new ApiResponse(404));
+            var thing = _context.Products.Find(42);
+
+            if (thing == null)
+            {
+                return NotFound(new ApiResponse(404));
+            }
+
+            return Ok();
         }
 
         [HttpGet("servererror")]
         public ActionResult GetServerError()
         {
-            var thing = _context.Products.Find(32);
-            var thingto = thing.ToString();
+            var thing = _context.Products.Find(42);
+
+            var thingToReturn = thing.ToString();
+
             return Ok();
         }
 
         [HttpGet("badrequest")]
         public ActionResult GetBadRequest()
         {
-            return NotFound(new ApiResponse(400));
+            return BadRequest(new ApiResponse(400));
         }
 
         [HttpGet("badrequest/{id}")]
         public ActionResult GetNotFoundRequest(int id)
         {
-            return NotFound(new ApiResponse(404));
+            return Ok();
         }
-
 
     }
 }
